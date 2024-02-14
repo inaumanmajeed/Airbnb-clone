61299e495c65f328c56b52ecd6f810e0e3661e01
inaumanmajeed <inaumanmajeed@gmail.com>
2024-02-15 00:44:00 +0500

    some text

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-$("body").scroll(alert("hey"))
-// cljzjkcb zc
-// zscjzkjcbkjzcx
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scrollval = $(window).scrollTop();
+        
+        if (scrollval >= 50) {
+
+            $(".navbar-menu").hide();
+
+            $(".scroll-bar").show().animate({width: '50%'}, 'slow');
+        } else {
+            $(".navbar-menu").show();
+            $(".scroll-bar").hide().css('width', '');
+        }
+    });
+});
